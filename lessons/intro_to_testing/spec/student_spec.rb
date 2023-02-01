0b11e63b7085dd9163afca19de922c958210dbb4
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-01 13:01:22 -0500

    Add initial tests and code for Student class

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,37 @@
-# Add the 2 requires you will need here
+# Add 2 requires needed here
+require 'rspec'
+require './lib/student'
 
+# Domain Specific Language (DSL)
 
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
-      # write the code to initialize a new student object
+      student = Student.new('Penelope')
+
+      expect(student).to be_instance_of(Student)
     end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
+
+    it 'has attributes' do
+      student = Student.new('Penelope')
+      student_2 = Student.new('Logan')
+
+      expect(student.name).to eq('Penelope')
+      expect(student_2.name).to eq('Logan')
+      expect(student.cookies).to eq([])
+      # test it has a name
+      # test it has cookies
+    end
+  end
+
+  describe '#add_cookie' do
+    it 'adds a cookie to a students cookies array' do
+      student = Student.new('Penelope')
+
+      student.add_cookie('Chocolate Chunk')
+      student.add_cookie('Snickerdoodle')
+
+      expect(student.cookies).to eq(['Chocolate Chunk', 'Snickerdoodle'])
+    end
   end
 end
