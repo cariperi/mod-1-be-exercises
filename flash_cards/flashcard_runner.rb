302e5d5212d0b727933abdf7cdd5186c47177954
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-02 13:55:10 -0500

    Update runner file to use cards from file

--- flash_cards/flashcard_runner.rb
+++ flash_cards/flashcard_runner.rb
@@ -2,13 +2,16 @@ require './lib/card.rb'
 require './lib/deck.rb'
 require './lib/turn.rb'
 require './lib/round.rb'
+require './lib/card_generator.rb'
 
-card_1 = Card.new('What is the capital of New York?', 'Albany', :state_capitals)
-card_2 = Card.new('What is the capital of Pennsylvania?', 'Harrisburg', :state_capitals)
-card_3 = Card.new('Who was the first US President?', 'Washington', :presidents)
-card_4 = Card.new('Who was the 16th US President?', 'Lincoln', :presidents)
+# card_1 = Card.new('What is the capital of New York?', 'Albany', :state_capitals)
+# card_2 = Card.new('What is the capital of Pennsylvania?', 'Harrisburg', :state_capitals)
+# card_3 = Card.new('Who was the first US President?', 'Washington', :presidents)
+# card_4 = Card.new('Who was the 16th US President?', 'Lincoln', :presidents)
 
-cards = [card_1, card_2, card_3, card_4]
+# cards = [card_1, card_2, card_3, card_4]
+filename = "cards.txt"
+cards = CardGenerator.new(filename).cards
 
 deck = Deck.new(cards)
 
