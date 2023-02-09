abff907b0183fa8e6fb8f1e21fd191f9b5759e38
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-09 15:40:30 -0500

    refactor: update turn spec with before hook

--- flash_cards/spec/turn_spec.rb
+++ flash_cards/spec/turn_spec.rb
@@ -3,51 +3,49 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+
+  before(:each) do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
+
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Portland", card)
+    turn = Turn.new("Portland", @card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Portland", card)
+    turn = Turn.new("Portland", @card)
 
     expect(turn.guess).to eq("Portland")
   end
 
   it 'has a current flashcard' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Portland", card)
+    turn = Turn.new("Portland", @card)
 
-    expect(turn.card).to eq(card)
+    expect(turn.card).to eq(@card)
   end
 
   it 'returns false when guess is wrong' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Portland", card)
+    turn = Turn.new("Portland", @card)
 
     expect(turn.correct?).to be false
   end
 
   it 'returns true when guess is right' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+    turn = Turn.new("Juneau", @card)
 
     expect(turn.correct?).to be true
   end
 
   it 'provides feedback on correct guesses' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+    turn = Turn.new("Juneau", @card)
 
     expect(turn.feedback).to eq("Correct!")
   end
 
   it 'provides feedback on incorrect guesses' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Portland", card)
+    turn = Turn.new("Portland", @card)
 
     expect(turn.feedback).to eq("Incorrect.")
   end
