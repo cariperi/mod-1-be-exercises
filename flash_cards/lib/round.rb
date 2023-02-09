22048ee3bf93d86b08c9418c7d45e2d2509ba0d8
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-09 16:42:51 -0500

    refactor: edit play method to use helper methods

--- flash_cards/lib/round.rb
+++ flash_cards/lib/round.rb
@@ -1,6 +1,5 @@
 class Round
-  attr_reader :deck, :turns
-  attr_accessor :current_card, :number_correct
+  attr_reader :deck, :turns, :current_card, :number_correct
 
   def initialize(deck)
     @deck = deck
@@ -17,12 +16,16 @@ class Round
     current_turn
   end
 
-  def pull_next_card  #come back to this
-    @current_card = deck.cards[@turns.count] 
+  def pull_next_card
+    @current_card = deck.cards[@turns.count]
   end
 
   def number_correct_by_category(category)
-    @turns.select{|turn| turn.card.answer == turn.guess && turn.card.category == category }.count
+    @turns.select{ |turn| turn.correct? && turn.card.category == category }.count
+  end
+
+  def turns_by_category(category)
+    @turns.select{ |turn| turn.card.category == category }.count
   end
 
   def percent_correct
@@ -30,15 +33,23 @@ class Round
   end
 
   def percent_correct_by_category(category)
-    number_correct = number_correct_by_category(category)
-    total_category = @turns.select{|turn| turn.card.category == category}.count
-    number_correct.fdiv(total_category)*100
+    (number_correct_by_category(category)).fdiv(turns_by_category(category))*100
+  end
+
+  def print_scores_by_category
+    deck.cards.map { |card| card.category }.uniq.each do |category|
+      puts "#{format_category(category)} - #{percent_correct_by_category(category).round()}% correct"
+    end
+  end
+
+  def format_category(category)
+    category.to_s.gsub("_", " ").capitalize
   end
 
   def start
     puts "Welcome! You're playing with #{deck.cards.count} cards."
     puts '-------------------------------------------------'
-    
+
     while @turns.count < deck.cards.count do
       puts "This is card number #{deck.cards.find_index(@current_card) + 1} out of #{deck.cards.count}."
       puts "Question: #{@current_card.question}"
@@ -49,12 +60,7 @@ class Round
 
     puts '****** Game over! ******'
     puts "You had #{@number_correct} correct guesses out of #{deck.cards.count} for a total score of #{percent_correct.round()}%."
-    categories = []
-    deck.cards.each do |card|
-      categories << card.category
-    end
-    categories.uniq.each do |category|
-      puts "#{category.to_s.gsub("_", " ").capitalize} - #{percent_correct_by_category(category).round()}% correct"
-    end
+
+    print_scores_by_category
   end
 end
