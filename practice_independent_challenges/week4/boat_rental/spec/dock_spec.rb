b19d76e1a9b9ede506d116d386ec8c6a86423a18
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-17 13:36:32 -0500

    test: add test for initialize method in Dock class

--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Dock do
+  before(:each) do
+    @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
+  end
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(@dock).to be_a Dock
+    end
+
+    it 'has a name' do
+      expect(@dock.name).to eq('The Rowing Dock')
+    end
+
+    it 'has a max rental time' do
+      expect(@dock.max_rental_time).to eq(3)
+    end
+  end
 end
