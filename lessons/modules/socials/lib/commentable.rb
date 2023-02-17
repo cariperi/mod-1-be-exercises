787add62c708e3064494f798d1522d11ce9f0d5f
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-17 12:49:34 -0500

    refactor: create commentable module

--- lessons/modules/socials/lib/commentable.rb
+++ lessons/modules/socials/lib/commentable.rb
@@ -1,4 +1,41 @@
 # ./commentable.rb
 module Commentable
-  # what extracted code goes here?
+  def add_comment(comment)
+		comments[next_comment_id] = comment
+	end
+
+	def remove_comment(id)
+		comments.delete(id)
+	end
+
+	def next_comment_id
+		if total_comments == 0
+			total_comments + 1
+		else
+			comments.keys.last + 1
+		end
+	end
+
+	def total_comments
+		comments.count
+	end
+
+  def display
+    if self.class == StatusUpdate
+      display_string = "-" * 50 + "\n"
+      display_string += "STATUS UPDATE" + "\n"
+      display_string += "Body: #{body}" + "\n"
+    elsif self.class == Photo
+      display_string = "-" * 50 + "\n"
+      display_string += "PHOTO" + "\n"
+      display_string += "URL: #{image_source}" + "\n"
+      display_string += "Caption: #{caption}" + "\n"
+    end
+
+      comments.each do |id, text|
+			display_string += "#{id}: #{text}" + "\n"
+		  end
+		  display_string += "-" * 50
+      display_string
+  end
 end
