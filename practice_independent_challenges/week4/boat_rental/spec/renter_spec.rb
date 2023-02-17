998c231b46ce2db039220b1e9f9cd34695e98851
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-17 13:29:57 -0500

    test: add test for Renter initialize method

--- practice_independent_challenges/week4/boat_rental/spec/renter_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/renter_spec.rb
@@ -1,1 +1,21 @@
 require 'spec_helper'
+
+describe Renter do
+  before(:each) do
+    @renter = Renter.new("Patrick Star", "4242424242424242")
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@renter).to be_a Renter
+    end
+
+    it 'has a name' do
+      expect(@renter.name).to eq('Patrick Star')
+    end
+
+    it 'has a credit card number' do
+      expect(@renter.credit_card_number).to eq('4242424242424242')
+    end
+  end
+end
