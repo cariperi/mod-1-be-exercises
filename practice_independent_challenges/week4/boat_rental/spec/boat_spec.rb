08e9d7e038f0bdc9b95bef4002425dc72d9e7446
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-17 13:22:22 -0500

    test: add test for initialize method

--- practice_independent_challenges/week4/boat_rental/spec/boat_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/boat_spec.rb
@@ -1,1 +1,25 @@
 require 'spec_helper'
+
+describe Boat do
+  before(:each) do
+    @kayak = Boat.new(:kayak, 20)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@kayak).to be_a Boat
+    end
+
+    it 'has a type' do
+      expect(@kayak.type).to eq(:kayak)
+    end
+
+    it 'has a price per hour' do
+      expect(@kayak.price_per_hour).to eq(20)
+    end
+
+    it 'starts with no hours rented by default' do
+      expect(@kayak.hours_rented).to eq(0)
+    end
+  end
+end
