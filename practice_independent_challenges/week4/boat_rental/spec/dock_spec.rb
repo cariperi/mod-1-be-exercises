417dff5bc1c047f0405002b6999111ff54306f27
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-17 14:06:10 -0500

    test: update test for rent method

--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -3,24 +3,45 @@ require 'spec_helper'
 describe Dock do
   before(:each) do
     @dock = Dock.new("The Rowing Dock", 3)
-    @kayak_1 = Boat.new(:kayak, 20)
-    @kayak_2 = Boat.new(:kayak, 20)
-    @sup_1 = Boat.new(:standup_paddle_board, 15)
-    @patrick = Renter.new("Patrick Star", "4242424242424242")
-    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
 
   describe '#initialize' do
-    it 'exists' do
-      expect(@dock).to be_a Dock
+  it 'exists' do
+    expect(@dock).to be_a Dock
+    expect(@dock.rental_log).to eq({})
+  end
+
+  it 'has a name' do
+    expect(@dock.name).to eq('The Rowing Dock')
+  end
+
+  it 'has a max rental time' do
+    expect(@dock.max_rental_time).to eq(3)
+  end
+end
+
+  describe '#rent' do
+    before(:each) do
+      @kayak_1 = Boat.new(:kayak, 20)
+      @kayak_2 = Boat.new(:kayak, 20)
+      @sup_1 = Boat.new(:standup_paddle_board, 15)
+      @patrick = Renter.new("Patrick Star", "4242424242424242")
+      @eugene = Renter.new("Eugene Crabs", "1313131313131313")
     end
 
-    it 'has a name' do
-      expect(@dock.name).to eq('The Rowing Dock')
-    end
+    it 'can allow a renter to rent a specific boat' do
+      expect(@dock.rental_log).to eq({})
 
-    it 'has a max rental time' do
-      expect(@dock.max_rental_time).to eq(3)
+      @dock.rent(@kayak_1, @patrick)
+      @dock.rent(@kayak_2, @patrick)
+      @dock.rent(@sup_1, @eugene)
+
+      expect(@dock.rental_log).to be_a Hash
+      expect(@dock.rental_log.keys).to eq([@kayak_1, @kayak_2, @sup_1 ])
+      expect(@dock.rental_log.values).to eq([@patrick, @eugene])
+      expect(@dock.rental_log[@kayak_1]).to eq(@patrick)
+      expect(@dock.rental_log[@kayak_2]).to eq(@patrick)
+      expect(@dock.rental_log[@sup_1]).to eq(@eugene)
     end
   end
 end
