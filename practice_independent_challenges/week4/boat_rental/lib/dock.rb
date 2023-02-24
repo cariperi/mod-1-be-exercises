22e9acbca71ee0a6a1c95387e5dd57328af5f93e
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-23 22:46:18 -0500

    feat: add charge method and get_hours helper method

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -12,4 +12,20 @@ class Dock
   def rent(boat, renter)
     @rental_log[boat] = renter
   end
+
+  def charge(boat)
+    charges = {}
+    charges[:card_number] = @rental_log[boat].credit_card_number
+    charges[:amount] = (get_hours(boat) * boat.price_per_hour)
+    charges
+  end
+
+  def get_hours(boat)
+    if boat.hours_rented > @max_rental_time
+      hours = @max_rental_time
+    else
+      hours = boat.hours_rented
+    end
+    hours
+  end
 end
