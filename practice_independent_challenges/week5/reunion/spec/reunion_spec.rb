ab2fafb4812c6d15dc02c138573be8ac83e97b60
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-06 21:25:02 -0500

    test: add test for initialize method to reunion spec

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper'
 
 describe Reunion do
   before(:each) do
-    @activity = Activity.new("Brunch")
+    @reunion = Reunion.new("1406 BE")
+    @activity_1 = Activity.new("Brunch")
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@reunion).to be_a Reunion
+    end
+
+    it 'has a name' do
+      expect(@reunion.name).to eq("1406 BE")
+    end
   end
 end
