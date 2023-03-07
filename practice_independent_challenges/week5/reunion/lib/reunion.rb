87465cfb23a2685d30168be4c05b319b11c81f90
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-06 21:48:56 -0500

    feat: add settle up method

--- practice_independent_challenges/week5/reunion/lib/reunion.rb
+++ practice_independent_challenges/week5/reunion/lib/reunion.rb
@@ -14,4 +14,14 @@ class Reunion
   def total_cost
     @activities.sum(&:total_cost)
   end
+
+  def settle_up
+    settle_up = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.each do |name, amount|
+        settle_up[name] += amount
+      end
+    end
+    settle_up
+  end
 end
