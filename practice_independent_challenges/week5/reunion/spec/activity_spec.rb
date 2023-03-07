d6750f0ca305de907ddc0ec106aab44a5b7bd99d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-06 21:06:42 -0500

    test: add initialize tests

--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Activity do
+  before(:each) do
+    @activity = Activity.new("Brunch")
+  end
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(@activity).to be_a Activity
+    end
+
+    it 'has attributes' do
+      expect(@activity.name).to eq("Brunch")
+      expect(@activity.participants).to eq({})
+    end
+  end
 end
