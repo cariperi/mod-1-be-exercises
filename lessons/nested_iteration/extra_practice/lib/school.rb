e75e932307a9c47943960ff192f42e3f4b257934
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-09 15:36:25 -0500

    add solution to nested iteration extra practice

--- lessons/nested_iteration/extra_practice/lib/school.rb
+++ lessons/nested_iteration/extra_practice/lib/school.rb
@@ -14,4 +14,32 @@ class School
   def assign_to_group(student)
     student.group = @groups.sample
   end
+
+  def group_all_students
+    @courses.each do |course|
+      course.students.each do |student|
+        student.group = @groups.sample
+      end
+    end
+  end
+
+  def names
+    @courses.map do |course|
+      course.students.map do |student|
+        student.name
+      end
+    end.flatten.uniq
+  end
+
+  def students_by_group
+    grouped_students = {}
+    @groups.each do |group|
+      students = []
+      @courses.each do |course|
+        students << course.students.find_all{|student| student.group == group}
+      end
+      grouped_students[group] = students.flatten.uniq
+    end
+    grouped_students
+  end
 end
